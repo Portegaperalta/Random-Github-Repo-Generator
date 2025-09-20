3a761a8373f966e5f0409e845d88580f850add0f
Pablo <ortegaperaltap@gmail.com>
2025-09-20 01:42:31 -0400

    add conditional styles to language select button and language list

--- app/src/components/LanguageSelect.tsx
+++ app/src/components/LanguageSelect.tsx
@@ -12,18 +12,22 @@ export default function LanguageSelect() {
 
   return (
     <div className="language-select flex flex-col">
-      <div className="language-select-button flex flex-row items-center
-      justify-between py-2 px-4 w-full rounded-t-lg border-2 border-b-0 
-      cursor-pointer">
-        <p className="text-[1.1rem]">
+      <div
+        onClick={handleSelectButtonClick}
+        className={`language-select-button flex flex-row items-center
+        justify-between py-2 px-4 w-full rounded-t-lg border-2 
+        cursor-pointer ${optionsDisplay ? `border-0` : `border-b-2`}`}>
+        <p className="text-[1.1rem] select-none">
           Select a Language
         </p>
         <ChevronDown />
       </div>
       <ul
         id="languageSelect"
-        className="text-[1.1rem] w-full h-60 rounded-b-lg border-2 
-        border-t-0 cursor-pointer scroll overflow-y-auto"
+        className={`text-[1.1rem] w-full h-60 rounded-b-lg border-2 
+        border-t-0 cursor-pointer scroll overflow-y-auto
+        ${optionsDisplay ? `inline-block` : `hidden`}
+        `}
       >
         {
           languages.map((language) => (
