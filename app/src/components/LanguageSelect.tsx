30d7b82eb700da3b76d52be449ffa9261c29c3f8
Pablo <ortegaperaltap@gmail.com>
2025-09-20 01:13:18 -0400

    change select element to ul for more customization and add overflow-y-auto to allow vertical scrolling

--- app/src/components/LanguageSelect.tsx
+++ app/src/components/LanguageSelect.tsx
@@ -3,22 +3,22 @@ import languages from "../data/languages"
 export default function LanguageSelect() {
   return (
     <div className="language-select">
-      <select
-        name="laguageSelect"
+      <ul
         id="languageSelect"
-        className="text-[1.1rem] px-4 py-2 w-full rounded-lg
-        border-2"
+        className="text-[1.1rem] px-4 py-2 w-full h-60 rounded-sm
+        border-2 cursor-pointer scroll overflow-y-auto"
       >
         {
           languages.map((language) => (
-            <option
+            <li
               key={language.title}
-              value={language.value}>
+              className="cursor-pointer"
+            >
               {language.title}
-            </option>
+            </li>
           ))
         }
-      </select>
+      </ul>
     </div>
   )
 }
