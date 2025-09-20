0c7ac7d6ede05588ef7e089701483359f79f30bc
Pablo <ortegaperaltap@gmail.com>
2025-09-20 01:01:00 -0400

    style select element

--- app/src/components/LanguageSelect.tsx
+++ app/src/components/LanguageSelect.tsx
@@ -3,7 +3,12 @@ import languages from "../data/languages"
 export default function LanguageSelect() {
   return (
     <div className="language-select">
-      <select name="laguageSelect" id="languageSelect">
+      <select
+        name="laguageSelect"
+        id="languageSelect"
+        className="text-[1.1rem] px-4 py-2 w-full rounded-lg
+        border-2"
+      >
         {
           languages.map((language) => (
             <option
