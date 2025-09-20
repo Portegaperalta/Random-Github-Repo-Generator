ae96946dad53be9f0415ef77fc6e4013a282b7a3
Pablo <ortegaperaltap@gmail.com>
2025-09-19 23:46:15 -0400

    add flexbox to title and reduce image size

--- app/src/components/RepoGenerator.tsx
+++ app/src/components/RepoGenerator.tsx
@@ -1,8 +1,12 @@
 export default function RepoGenerator() {
   return (
     <div className="repo-generator">
-      <div className="repo-generator-title">
-        <img src="/github-tile.svg" alt="github logo" />
+      <div className="repo-generator-title flex items-center">
+        <img
+          src="/github-tile.svg"
+          alt="github logo"
+          className="w-full max-w-10"
+        />
         <h1>GitHub Repository Finder</h1>
       </div>
     </div>
