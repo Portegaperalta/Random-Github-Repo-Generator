005db23e58fee100a662ebb446e6fed25483bbc6
Pablo <ortegaperaltap@gmail.com>
2025-09-22 22:17:37 -0400

    remove RepoDisplayProps for restructuring purposes

--- app/src/components/RepoDisplay.tsx
+++ app/src/components/RepoDisplay.tsx
@@ -1,15 +1,6 @@
 import { Star, GitFork, CircleAlert } from "lucide-react"
 
-type RepoDisplayProps = {
-  fullName: string,
-  description: string,
-  language: string,
-  totalStars: number,
-  totalForks: number,
-  totalIssues: number,
-}
-
-export default function RepoDisplay({ fullName, description, language, totalStars, totalForks, totalIssues }: RepoDisplayProps) {
+export default function RepoDisplay() {
   return (
     <div className="repo-display bg-(--clr-gray) py-6 px-4 rounded-lg">
       <div className="status-text text-center">
@@ -20,18 +11,18 @@ export default function RepoDisplay({ fullName, description, language, totalStar
       <div className="repo-content space-y-6 hidden">
         <div className="repo-title">
           <h2 className="text-[1.2rem]">
-            {fullName}
+
           </h2>
         </div>
         <div className="repo-description">
           <p>
-            {description}
+
           </p>
         </div>
         <div className="repo-info flex justify-between">
           <div className="repo-language flex">
             <p>
-              {language}
+
             </p>
           </div>
           <div
@@ -39,21 +30,21 @@ export default function RepoDisplay({ fullName, description, language, totalStar
             className="repo-stars text-(--clr-gray-text) 
             flex gap-1 cursor-default">
             <Star size={18} />
-            <p>{totalStars}</p>
+            <p></p>
           </div>
           <div
             title="Forks"
             className="repo-forks text-(--clr-gray-text) 
             flex gap-1 cursor-default">
             <GitFork size={18} />
-            <p>{totalForks}</p>
+            <p></p>
           </div>
           <div
             title="Issues"
             className="repo-issues text-(--clr-gray-text) 
             flex gap-1 cursor-default">
             <CircleAlert size={18} />
-            <p>{totalIssues}</p>
+            <p></p>
           </div>
         </div>
       </div>
