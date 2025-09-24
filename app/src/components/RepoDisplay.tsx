8db7dc4bc3c8ef496886981d775a191e7e38564a
Pablo <ortegaperaltap@gmail.com>
2025-09-24 19:37:01 -0400

    wrap RepoDisplay content inside a link to the original github page

--- app/src/components/RepoDisplay.tsx
+++ app/src/components/RepoDisplay.tsx
@@ -10,55 +10,57 @@ export default function RepoDisplay(props: RepoDisplayProps) {
   return (
     props.repoData ? (
       <div className="repo-display bg-(--clr-gray) py-6 px-4 rounded-lg">
-        <div className="repo-content space-y-6">
-          <div className="repo-title">
-            <a
-              href={props.repoData.html_url}
-              target="about_blank"
-              className="text-[1.2rem] hover:underline">
-              {props.repoData.full_name}
-            </a>
-          </div>
-          <div className="repo-description">
-            <p>
-              {props.repoData.description}
-            </p>
-          </div>
-          <div className="repo-info flex justify-between">
-            <div className="repo-language flex">
+        <a
+          href={props.repoData.html_url}
+          target="about_blank"
+        >
+          <div className="repo-content space-y-6">
+            <div className="repo-title">
+              <h2 className="text-[1.2rem] hover:underline">
+                {props.repoData.full_name}
+              </h2>
+            </div>
+            <div className="repo-description">
               <p>
-                {props.repoData.language}
+                {props.repoData.description}
               </p>
             </div>
-            <div
-              title="Stars"
-              className="repo-stars text-(--clr-gray-text) 
+            <div className="repo-info flex justify-between">
+              <div className="repo-language flex">
+                <p>
+                  {props.repoData.language}
+                </p>
+              </div>
+              <div
+                title="Stars"
+                className="repo-stars text-(--clr-gray-text) 
             flex gap-1 cursor-default">
-              <Star size={18} />
-              <p>
-                {props.repoData.stargazers_count}
-              </p>
-            </div>
-            <div
-              title="Forks"
-              className="repo-forks text-(--clr-gray-text) 
+                <Star size={18} />
+                <p>
+                  {props.repoData.stargazers_count}
+                </p>
+              </div>
+              <div
+                title="Forks"
+                className="repo-forks text-(--clr-gray-text) 
             flex gap-1 cursor-default">
-              <GitFork size={18} />
-              <p>
-                {props.repoData.forks_count}
-              </p>
-            </div>
-            <div
-              title="Issues"
-              className="repo-issues text-(--clr-gray-text) 
+                <GitFork size={18} />
+                <p>
+                  {props.repoData.forks_count}
+                </p>
+              </div>
+              <div
+                title="Issues"
+                className="repo-issues text-(--clr-gray-text) 
             flex gap-1 cursor-default">
-              <CircleAlert size={18} />
-              <p>
-                {props.repoData.open_issues_count}
-              </p>
+                <CircleAlert size={18} />
+                <p>
+                  {props.repoData.open_issues_count}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
     ) : (
       <div className={`repo-display py-6 px-4 rounded-lg 
