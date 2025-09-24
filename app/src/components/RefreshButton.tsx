ae54d921e85c80f23781bcfc79d02225162bacff
Pablo <ortegaperaltap@gmail.com>
2025-09-24 17:46:25 -0400

    create props for RefreshButton component

--- app/src/components/RefreshButton.tsx
+++ app/src/components/RefreshButton.tsx
@@ -1,3 +1,8 @@
+type RefreshButtonProps = {
+  className: string,
+  onClick: () => void,
+}
+
 export default function RefreshButton() {
   return (
     <button className="refresh-button text-white text-center py-2
